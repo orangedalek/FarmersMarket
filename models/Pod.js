0d361e0a09000684e4c1c8fbe5052c873cb519fe
Charlotte Astle <charlotteastle@charlottes-mbp.attlocal.net>
2017-08-20 19:30:54 -0700

    creates models with mongoose associations

---
+++ models/Pod.js
@@ -0,0 +1,23 @@
+var mongoose = require('mongoose'),
+  , Schema = mongoose.Schema
+
+var podSchema = Schema ({
+	title: {
+		type: String,
+		null: false
+	},
+	icon: {
+		type: String
+	},
+	description: {
+		type: String
+	},
+	members: [{
+		// This Number will be equal to the User _id
+		type: Number,
+		ref: User
+	}]
+});
+
+var Pod = mongoose.model('Pod', podSchema);
+module.exports = Pod;
