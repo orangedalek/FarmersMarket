6cdab43e7f684cb680cadd710ccf0687748cb263
jennie snider <jenniesnider@calvisitor-10-105-142-87.calvisitor.1918.berkeley.edu>
2017-08-21 11:54:02 -0700

    index, app and main

---
+++ app/main.js
@@ -0,0 +1,48 @@
+
+var React = require("react");
+
+//subcomponents go here, not sure what they'll be called yet
+// var ??? = require("./???");
+
+
+var helpers = require("../utils/helpers");
+
+
+var Main = React.createClass({
+
+	getInitialState: function(){
+		return {
+			//need to see models for key names
+		};
+	},
+
+	//functions to load user data, handle clicks...
+
+	render: function(){
+		return(
+			<div className="container">
+				<div className="navbar navbar-default">
+					<div className="navbar-header">
+						<a className="navbar-brand" href="#">PodSquad</a>
+					</div>
+					 <ul className="nav navbar-nav navbar-right">
+					 	<li><a href="#">dashboard</a></li>
+					 	<li><a href="#">search</a></li>
+					 </ul>
+				</div>
+				<div className="row">
+					<div className="col-md-8" id="pods">
+						<Pods />
+
+					</div>
+					<div className="col-md-4">
+						<You />
+						<YourPods />
+					</div>
+				</div>
+			</div>
+
+			)
+	} 
+
+})
