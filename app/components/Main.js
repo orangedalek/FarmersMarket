d02ae080b5e24ddabefad6e2b3aa006d0d3f10a1
jennie snider <jenniesnider@jennies-MacBook-Pro.local>
2017-08-22 13:22:23 -0700

    updating dashboard

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,57 +1,39 @@
-var axios = require('axios');
 var React = require('react');
+var Link = require("react-router").Link; 
 //subcomponents go here, not sure what they'll be called yet
 // var ??? = require("./???");
-var router = require('react-router-dom');
-var Link = router.Link;
-var Route = router.Route;
-
-//include all the sub-components
-var dashboard = require("./children/dashboard");
-var newUser = require("./children/newUser");
-var podLanding = require("./children/podLanding");
-
-var helpers = require('./utils/helpers.js');
-
+var helpers = require('../utils/helpers');
 var Main = React.createClass({
     getInitialState: function(){
         return {
             //need to see models for key names
-
         };
     },
     //functions to load user data, handle clicks...
-    render: function() {
+    render: function(){
         return(
             <div className="container">
+                <div className="navbar navbar-default">
+                    <div className="navbar-header">
+                        <a className="navbar-brand" href="#">PodSquad</a>
+                    </div>
+                     <ul className="nav navbar-nav navbar-right">
+                        <li><a href="#">dashboard</a></li>
+                        <li><a href="#">search</a></li>
+                     </ul>
+                </div>
                 <div className="row">
-                  <div className="jumbotron text-center" style={{'backgroundImage': 'url(./assets/img/background.jpg)', 'backgroundRepeat': 'no-repeat', 'backgroundPosition': 'center', 'backgroundSize': '100% 100%', 'backgroundAttachment': 'fixed'}}>
-                    <h2 className="text-center"style={{'color': 'white', 'fontWeight': 'bold', 'fontSize': '48px'}}>PodSquad</h2>
-                    <p className="text-center" style={{'color': 'white'}}>
-                      <em>Search for an podcast and join!</em>
-                    </p>
-                    <hr />
-                    <p>
-                    <Link to="/"><button className="btn btn-primary btn-lg">Search Podcasts</button></Link>
-                    <Link to="/podLanding"><button className="btn btn-danger btn-lg">Pod Landing</button></Link>
-                    </p>
-                  </div>
-                  </div>
-
-                   <div className="row">
-                     
-                        <Route exact path="/" render={(props) => (
-                           <dashboard {...props} />
-                        )} />
-
-                        <Route exact path="/podLanding" render={(props) => (
-                            <podLanding {...props} />
-                        )} />
-                 </div>
-
-          </div>        
-     );
-   }
+                    <div className="col-md-8" id="pods">
+                        <Pods />
+                    </div>
+                    <div className="col-md-4">
+                        <You />
+                        <YourPods />
+                    </div>
+                </div>
+            </div>
+            )
+    } 
 });
 
 module.exports = Main;
--- app/components/children/dashboard.js
+++ app/components/children/dashboard.js
@@ -13,27 +13,52 @@ var dashboard = React.createClass({
 
 	render: function() {
 		return (
-			<div>
-				<div className="col-md-12">
-					<div className="panel panel-default">
-						<div className="panel-heading">
-							<h2 className="panel-title text-center">Your Space</h2>
-						</div>
-						<div className="panel-body text-center">
-							<form>
-								<div className="form-group">
-									<h4 className=""><strong>Search Term</strong></h4>
-									<input type="text" className="form-control text-center" id="title" placeholder="What topic would you like to search?" onChange= {this.handleChange} required/>
-									<br />
-										
-									<button type="button" className="btn btn-primary" onClick={this.handleClick}>Search</button>
-								</div>
-							</form>
-						</div>
-				            	
-				    </div>
+			
+	<div className="container">
+		<nav className="navbar navbar-default">
+			<div className="navbar-header">
+				<a className="navbar-brand" href="#">PodSquad</a>
+			</div>
+			<div className="nav navbar-right">
+				<a id="dashLink" href="#">dashboard</a>
+				<form className="navbar-form" role="search">
+  				<div className="form-group">
+    			<input type="text" className="form-control text-center" id="title" placeholder="search" onChange= {this.handleChange} required/>
+  				</div>
+  				<button type="submit" className="btn btn-default" onClick={this.handleClick}>Search</button>
+				</form>
+			</div>
+		</nav>
+		<div className="row">
+			<div className="col-md-8">
+				<div className="panel panel-default">
+					<div className="panel-heading">
+						<h2 className="panel-title text-center">Your Pods</h2>
+					</div>
+					<div className="panel-body">
+						<h1>PODS GO HERE</h1>
+					</div>
 				</div>
 			</div>
+			<div className="col-md-4">
+				<div className="panel panel-default">
+					<div className="panel-heading">
+						<h2 className="panel-title text-center">You</h2>
+					</div>
+					<div className="panel-body">
+						<h2>Name</h2>
+						<img src="#">
+						<hr>
+						<h2>Pods</h2>
+						<ul>
+							<li>podname</li>
+							<li>podname</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 		)
 	}
 });
