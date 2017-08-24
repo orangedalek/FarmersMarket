44aac4f7cc9ac1d08c5429e7b38ca48cd66cefa1
Austin Alexander Lee <austin.alexander.lee@gmail.com>
2017-08-24 11:04:57 -0700

    adding Create/Podcast Search

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,27 +1,11 @@
-var axios = require('axios');
 var React = require('react');
+var Link = require("react-router").Link; 
 //subcomponents go here, not sure what they'll be called yet
 // var ??? = require("./???");
-var router = require('react-router-dom');
-var Link = router.Link;
-var Route = router.Route;
-
-//include all the sub-components
-var dashboard = require("./children/dashboard");
-var newUser = require("./children/newUser");
-var podLanding = require("./children/podLanding");
-
-var helpers = require('./utils/helpers.js');
-
+var helpers = require('../utils/helpers');
 var Main = React.createClass({
-    getInitialState: function(){
-        return {
-            //need to see models for key names
 
-        };
-    },
-    //functions to load user data, handle clicks...
-    render: function() {
+    render: function(){
         return(
             <div className="container">
                 <div className="row">
@@ -52,6 +36,56 @@ var Main = React.createClass({
           </div>
      );
    }
+
+                <div className="container">    
+        
+    <div id="loginbox" className="mainbox col-md-6 col-md-offset-3 col-sm-6 col-sm-offset-3"> 
+        
+        <div className="row">                
+            <div className="iconmelon">
+              <svg viewBox="0 0 32 32">
+                <g filter="">
+                  <use xlink:href="#git"></use>
+                </g>
+              </svg>
+            </div>
+        </div>
+        
+        <div className="panel panel-default" >
+            <div className="panel-heading">
+                <div className="panel-title text-center">PodSquad!</div>
+            </div>     
+
+            <div className="panel-body" >
+
+                <form name="form" id="form" className="form-horizontal" enctype="multipart/form-data" method="POST">
+                   
+                    <div className="input-group">
+                        <span className="input-group-addon"><i className="glyphicon glyphicon-user"></i></span>
+                        <input id="user" type="text" className="form-control" name="user" value="" placeholder="User" />                                        
+                    </div>
+
+                    <div className="input-group">
+                        <span className="input-group-addon"><i className="glyphicon glyphicon-lock"></i></span>
+                        <input id="password" type="password" className="form-control" name="password" placeholder="Password" />
+                    </div>                                                                  
+
+                    <div className="form-group">
+                        <div className="col-sm-12 controls">
+                            <button type="submit" href="#" className="btn btn-primary pull-right"><i className="glyphicon glyphicon-log-in"></i> Log in</button>                          
+                        </div>
+                    </div>
+
+                </form>     
+
+            </div>                     
+        </div>  
+    </div>
+</div>
+{this.props.children}
+    
+            )
+    } 
 });
 
 module.exports = Main;
--- app/components/children/dashboard.js
+++ app/components/children/dashboard.js
@@ -30,7 +30,6 @@ var dashboard = React.createClass({
 
 	render: function() {
 		return (
-
 			<nav className="navbar navbar-default">
 				<div className="container wrapper">
 					<div className="navbar-header">
@@ -69,7 +68,6 @@ var dashboard = React.createClass({
 					</div>
 
 
-
 					<div className="col-md-4">
 
 						<div className="row">
@@ -101,8 +99,93 @@ var dashboard = React.createClass({
 
 					</div>
 
+	<div class="container">
+		<nav id="topNav" class="navbar navbar-default navbar-fixed-top">
+	        <div class="container-fluid">
+	            <div class="navbar-header">
+	                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-navbar">
+	                    <span class="sr-only">Toggle navigation</span>
+	                    <span class="icon-bar"></span>
+	                    <span class="icon-bar"></span>
+	                    <span class="icon-bar"></span>
+	                </button>
+	                <a class="navbar-brand page-scroll" href="#first"><i class="ion-ios-analytics-outline"></i>PodSquad</a>
+	            </div>
+	            <div class="navbar-collapse collapse" id="bs-navbar">
+	                <ul class="nav navbar-nav">
+	                    <li>
+	                        <Link to="/about">About</Link>
+	                    </li>
+	                    <li>
+	                       <Link to="/dashboard">Dashboard</Link>
+	                    </li>
+	                    <li>
+	                        <Link to="/contact">Contact</Link>
+	                    </li>
+	                </ul>
+	                <ul class="nav navbar-nav navbar-right">
+	                    <li>
+	                        <form class="navbar-form" role="search">
+								<div class="form-group">
+								<input type="text" class="form-control" placeholder="Search" />
+								<button type="submit" class="btn btn-default" onClick={this.handleClick}>Search</button>
+								</div>
+							</form>
+	                    </li>
+	                </ul>
+	            </div>
+	        </div>
+	    </nav>
+	    <div class="spacer"></div>
+
+		<div class="row">
+			<div class="col-md-8">
+				<div class="panel panel-default">
+					<div class="panel-heading">
+						<h2 class="panel-title text-center">Your Pods</h2>
+					</div>
+					<div class="panel-body">
+						<h1>PODS GO HERE</h1>
+					</div>
 				</div>
 			</div>
+			<div class="col-md-4">
+				<div class="panel panel-default">
+					<div class="panel-heading">
+						<h2 class="panel-title text-center">You</h2>
+					</div>
+					<div class="panel-body">
+						<div class="container">
+							<div class="row">
+								<div class="col-md-2">
+									<h2>Name</h2>
+								</div>
+								<div class="col-md-2">
+									<img id="user-pic" src="Koala_climbing_tree.jpg"></img>	
+								</div>
+							</div>
+							<hr></hr>
+							<div>
+								<h2>Pods</h2>
+								<ul>
+									<li>podname</li>
+									<li>podname</li>
+								</ul>
+							</div>
+							<br></br>
+							<br></br>
+							<hr></hr>
+							<div id="create-btn-holder" class="row">
+								<div class="col-md-4">
+									<button id="create-btn" class="btn btn-default btn-round-sm btn-sm">Create New Pod!</button>
+								</div>
+							</div>	
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 		)
 	}
 });
