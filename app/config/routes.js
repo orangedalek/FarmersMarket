d07e9b02a87c927772b8de7e515d38e3350c356b
Charlotte A <charlotteoa@icloud.com>
2017-08-24 10:52:45 -0700

    fixes nested routes

--- app/config/routes.js
+++ app/config/routes.js
@@ -1,33 +1,52 @@
-var React = require('react');
-//Uses ES6 for importing router library
-var router = require("react-router-dom");
-
+// Inclue the React library
+var React = require("react");
+// Include the react-router module
+var router = require("react-router");
+// Include the Route component for displaying individual routes
 var Route = router.Route;
-
-var BrowserRouter = router.BrowserRouter;
-
+// Include the Router component to contain all our Routes
+// Here where we can pass in some configuration as props
+var Router = router.Router;
+// Include the hashHistory prop to handle routing client side without a server
+// https://github.com/ReactTraining/react-router/blob/master/docs/guides/Histories.md#hashhistory
+var hashHistory = router.hashHistory;
+// Include the IndexRoute (catch-all route)
+var IndexRoute = router.IndexRoute;
+// Reference the high-level components
 var Main = require('../components/Main');
 var dashboard = require('../components/children/dashboard');
 var newUser = require('../components/children/newUser');
+var login = require('../components/children/Login');
 var podLanding = require('../components/children/podLanding');
+var searchPodcasts = require('../components/children/searchPodcasts');
 //var results = require('../components/children/results');
-
-
 module.exports = (
-	// Router component
-	<BrowserRouter>
-
+	<Router>
 		<Route component={Main} />
+		<Route path='/dashboard' component={dashboard} />
+		<Route path='/newuser' component={newUser} />
+		<Route path='/login' component={Login} />
+		<Route path='/pod' component={podLanding} />
+		<Route path='/search' component={searchPodcasts} />
+	</Router>
+);
+// Export the Routes
+module.exports = (
+    // The high level component is the Router component
+    <Router history={hashHistory}>
+      <Route path="/" component={Main}>
+      
+        <Route path="/dashboard" component={dashboard} >
+ 
+          <Route path="/newUser" component={newUser} />
+          <Route path="/login" component={login} />
+        </Route>
 
-		<Route path='/dashboard' component={dashboard}/>
+        <Route path="/pod" component={podLanding} />
 
-		<Route path='/login' component={Login}/>
+        <Route path="/search" component={searchPodcasts} />
 
-		<Route path='/newuser' component={newUser}/>
-
-		<Route path='/pod' component={podLanding}/>
-
-		<Route path='/search' component={seachPodcast}/>
-
-	</BrowserRouter>
+        <IndexRoute component={Main} />
+      </Route>
+    </Router>
 );
