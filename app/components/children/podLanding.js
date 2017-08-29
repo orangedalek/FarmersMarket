9fbe04b45d52dba562b6d9af204c9510468afe11
Ken Wong <KenWong@Kens-MacBook-Pro.local>
2017-08-29 02:12:28 -0700

    updated pod landing

--- app/components/children/podLanding.js
+++ app/components/children/podLanding.js
@@ -14,36 +14,36 @@ var PodLanding = React.createClass({
 			  members: this.members,
 			  admin: this.admin
 			}
-		);
+		)
 	},
 
 	render: function() {
         return(
         <div className="container">
         
-            <div class="row">
-                <div class="col-md-8">
-                    <div class="panel panel-default">
-                        <div class="panel-body">
-                            <div class="row">
-                                <div class="col-md-8">
+            <div className="row">
+                <div className="col-md-8">
+                    <div className="panel panel-default">
+                        <div className="panel-body">
+                            <div className="row">
+                                <div className="col-md-8">
                                     <h1>{this.title}</h1>
                                 </div>
-                                <div class="col-md-4">
-                                    <img id="pod-image" src="../../public/assets/images/pod.jpg"></img>
+                                <div className="col-md-4">
+                                    <img id="pod-image" src="../../public/assets/images/pod.jpg" />
                                 </div>
                             </div>
-                            <div class="wrapper">
-                                <button id="joinBtn" class="btn btn-default btn-round-sm btn-sm">+Join</button>
+                            <div className="wrapper">
+                                <button id="joinBtn" className="btn btn-default btn-round-sm btn-sm">+Join</button>
                             </div>
-                            <div class="wrapper">
+                            <div className="wrapper">
                                 <h2>EPISODE</h2>
                             </div>
-                            <div class="row">
-                                <div class="col-md-4">
-                                    <img id="podcast-image" src=""></img>
+                            <div className="row">
+                                <div className="col-md-4">
+                                    <img id="podcast-image" src="" />
                                 </div>
-                                <div class="col-md-8">
+                                <div className="col-md-8">
                                     <h4>Show:</h4>
                                     <h4>Episode:</h4>
                                 </div>
@@ -52,23 +52,25 @@ var PodLanding = React.createClass({
                     </div>
                 </div>
             </div>
-            <form id="comment-form">
-              <div class="form-group">
-                <label for="comment-name">Name</label>
-                <input type="text" class="form-control" id="comment-name" placeholder="Name"></input>
-                <p class="validate" id="name-validate">Please enter your name.</p>
-              </div>
-              <div class="form-group">
-                <label for="comment-textarea">Comment</label>
-                <textarea class="form-control" id="comment-textarea" rows="3" placeholder="Comment"></textarea>
-                <p class="validate" id="comment-validate">Please enter a comment.</p>
-              </div>
-              <button type="button" class="btn btn-default" id="comment-save">Save</button>
-            </form>
+            <div className="panel panel-default">
+                <div className="panel-body">    
+                    <form id="comment-form">
+                      <div className="form-group">
+                        <label htmlFor="comment-name">Name</label>
+                        <input type="text" className="form-control" id="comment-name" placeholder="Name"></input>
+                        <p className="validate" id="name-validate">Please enter your name.</p>
+                      </div>
+                      <div className="form-group">
+                        <label htmlFor="comment-textarea">Comment</label>
+                        <textarea className="form-control" id="comment-textarea" rows="3" placeholder="Comment"></textarea>
+                        <p className="validate" id="comment-validate">Please enter a comment.</p>
+                      </div>
+                      <button type="button" className="btn btn-default" id="comment-save">Save</button>
+                    </form>
+                </div>
+            </div>
         </div>
-    );
-}
-
-})
+     )}
+});
 
 module.exports = PodLanding;
